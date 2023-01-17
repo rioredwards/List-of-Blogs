af3ee183441bf39a671bebcbbbe47f2dd4b9d982
Rio Edwards <rioredwards@gmail.com>
2023-01-17 14:44:09 -0800

    Add authors to each blog

--- src/components/BlogCard/BlogCard.js
+++ src/components/BlogCard/BlogCard.js
@@ -1,12 +1,13 @@
 import React from 'react';
 import './BlogCard.css';
 
-export default function BlogCard({ title, subtitle, text, image }) {
+export default function BlogCard({ title, authors, subtitle, text, image }) {
   return (
     <div className="blog-card">
       <div className="details">
         <div className="title">{title}</div>
         <div className="subtitle">{subtitle}</div>
+        <div className="author">{`Author: ${authors.name}`}</div>
         <div className="text">{text}</div>
       </div>
       <div className="image">
--- src/services/blogs.js
+++ src/services/blogs.js
@@ -1,6 +1,19 @@
 import { checkError, client } from './client';
 
+// export async function fetchBlogs() {
+//   const resp = await client.from('blogs').select('*');
+//   console.log(resp);
+//   return checkError(resp);
+// }
+
+// export async function fetchBlogs() {
+//   return await client
+//     .from('blogs')
+//     .order('created_at', { foreignTable: 'authors', ascending: false });
+// }
+
 export async function fetchBlogs() {
-  const resp = await client.from('blogs').select('*');
+  const resp = await client.from('blogs').select('*, authors(name)');
+  console.log('resp', resp);
   return checkError(resp);
 }
